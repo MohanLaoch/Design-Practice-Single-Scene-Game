87c40cf07916ae646cd1ff32e0040018bd9eccab
James Mohan <c20305011@mytudublin.ie>
2021-05-10 22:51:55 +0100

    Added Magical Gate

---
+++ 2D Single Design Practice Game Scene/Assets/Scripts/InteractableScript.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableScript : MonoBehaviour
+{
+   public Animator anim;
+   void OnTriggerEnter2D(Collider2D cologne)
+   {
+      if (cologne.gameObject.tag == "Player")
+      {
+         anim.SetTrigger("player");
+      }
+   }
+}
